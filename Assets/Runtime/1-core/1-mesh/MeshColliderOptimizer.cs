c9aecb68e3ad5be55519eda2c7af62cd4e972a63
Aniket Rajnish <rajnish.ankt@gmail.com>
2025-08-31 12:00:02 -0400

    [rmv] no strat using one method

--- Assets/Runtime/1-core/1-mesh/MeshColliderOptimizer.cs
+++ Assets/Runtime/1-core/1-mesh/MeshColliderOptimizer.cs
@@ -1,4 +1,6 @@
 ﻿using UnityEngine;
+using Habrador_Computational_Geometry;
+using HConnect = Habrador_Computational_Geometry.HalfEdgeData3.ConnectOppositeEdges;
 
 namespace UnityColliderOptimizer.Core.M
 {
@@ -7,7 +9,6 @@ namespace UnityColliderOptimizer.Core.M
     public class MeshColliderOptimizer : BaseColliderOptimizer<MeshCollider, Mesh>
     {
         [SerializeField] public MeshOptParams Params = new MeshOptParams();
-        IMeshSimpStrat _strat = new MeshSimpStratLegacy();
         Mesh _srcMesh
         {
             get
@@ -25,6 +26,7 @@ namespace UnityColliderOptimizer.Core.M
             UnityEditor.Undo.RecordObject(_tgtColl, "Reset MeshCollider to Authoring");
 #endif
             ResetColliderTo(src);
+
             var pSrc = Params ?? (Params = new MeshOptParams());
             var p = new MeshOptParams
             {
@@ -36,12 +38,13 @@ namespace UnityColliderOptimizer.Core.M
                 LossyScale = transform ? transform.lossyScale : Vector3.one
             };
 
-            var simplified = _strat.Simplify(src, p);
+            var simplified = SimplifyMesh(src, p);
+
             if (p.Convex && simplified && simplified.triangles != null)
             {
                 int triCount = simplified.triangles.Length / 3;
                 if (triCount > 255)
-                    Debug.LogWarning($"convex MeshCollider will be auto-reduced by Unity, tris={triCount} (>255)", this);
+                    Debug.LogWarning($"convex MeshCollider may be auto-reduced by Unity, tris={triCount} (>255)", this);
             }
 
 #if UNITY_EDITOR
@@ -67,6 +70,60 @@ namespace UnityColliderOptimizer.Core.M
             _tgtColl.sharedMesh = null;
             _tgtColl.sharedMesh = m;
             _saved = m;
+        }
+        static Mesh SimplifyMesh(Mesh __src, MeshOptParams __p)
+        {
+            var src = (__p.BakeScale && __p.LossyScale != Vector3.one)
+                ? BakeScaledCopy(__src, __p.LossyScale)
+                : __src;
+
+            var my = new MyMesh(src);
+            var norm = new Normalizer3(my.vertices);
+            my.vertices = norm.Normalize(my.vertices);
+
+            var he = new HalfEdgeData3(my, Map(__p.Connect));
+
+            int edgesApprox = Mathf.Max(1, he.faces.Count * 3 / 2);
+            int e2c = Mathf.Clamp(
+                Mathf.RoundToInt(Mathf.Clamp01(__p.ContractionFactor) * edgesApprox),
+                0, edgesApprox
+            );
+            if (e2c == 0) return CloneMesh(src);
+
+            var heOut = MeshSimplification_QEM.Simplify(
+                he,
+                maxEdgesToContract: e2c,
+                maxError: Mathf.Infinity,
+                normalizeTriangles: true
+            );
+
+            var outMy = heOut.ConvertToMyMesh("Simplified Mesh", MyMesh.MeshStyle.HardEdges);
+            outMy.vertices = norm.UnNormalize(outMy.vertices);
+
+            var outUnity = outMy.ConvertToUnityMesh(__p.RecalcNormals, "Simplified Collider");
+            outUnity.RecalculateBounds();
+            return outUnity;
+        }
+        static HConnect Map(ConnectOppositeEdges m) => m switch
+        {
+            ConnectOppositeEdges.Precise => HConnect.Precise,
+            ConnectOppositeEdges.Fast => HConnect.Fast,
+            _ => HConnect.No
+        };
+        static Mesh CloneMesh(Mesh __m)
+        {
+            var c = Object.Instantiate(__m);
+            c.name = __m.name + " (Clone)";
+            return c;
+        }
+        static Mesh BakeScaledCopy(Mesh __m, Vector3 __scale)
+        {
+            var c = CloneMesh(__m);
+            var v = c.vertices;
+            for (int i = 0; i < v.Length; i++) v[i] = Vector3.Scale(v[i], __scale);
+            c.vertices = v;
+            c.RecalculateBounds();
+            return c;
         }
         void ResetColliderTo(Mesh __src)
         {
--- Assets/Runtime/1-core/2-poly/IPolySimpStrat.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-namespace UnityColliderOptimizer.Core.P
-{
-    public interface IPolySimpStrat
-    {
-        List<List<Vector2>> Simplify(List<List<Vector2>> __src, PolyOptParams __p, Transform __t = null);
-    }
-}
