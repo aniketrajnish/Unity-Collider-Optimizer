2792c819c7316b76d3a40342baec43410fb4cfab
Aniket Rajnish <rajnish.ankt@gmail.com>
2025-09-02 00:36:47 -0400

    [update] reset to defaults

--- Assets/ColliderOptimizer/Editor/1-core/OptParams.cs
+++ Assets/ColliderOptimizer/Editor/1-core/OptParams.cs
@@ -14,6 +14,18 @@ namespace ColliderOptimizer.Core
         public Vector3 LossyScale = Vector3.one;
         public bool Aggressive = false;
         public bool Permissive = false;
+
+        public void CopyFrom(MeshOptParams __other)
+        {
+            ContractionFactor = __other.ContractionFactor;
+            RecalcNormals = __other.RecalcNormals;
+            BakeScale = __other.BakeScale;
+            Convex = __other.Convex;
+            LossyScale = __other.LossyScale;
+            Aggressive = __other.Aggressive;
+            Permissive = __other.Permissive;
+        }
+        public void ResetToDefaults() => CopyFrom(new MeshOptParams());
     }
     [System.Serializable]
     public class PolyOptParams
@@ -21,6 +33,14 @@ namespace ColliderOptimizer.Core
         public ToleranceMode Mode = ToleranceMode.World;
         public float Tolerance = .01f;
         public bool PerPathScaleByBounds = false;
+
+        public void CopyFrom(PolyOptParams __other)
+        {
+            Mode = __other.Mode;
+            Tolerance = __other.Tolerance;
+            PerPathScaleByBounds = __other.PerPathScaleByBounds;
+        }
+        public void ResetToDefaults() => CopyFrom(new PolyOptParams());
     }
 }
 #endif
